e88f083f6c0f88e6aed5907afe06af16bd219919
Jeff Sandberg <jeff@pdx.su>
2026-09-22 22:41:36 -0600

    Init

---
+++ lib/lucidex.ex
@@ -0,0 +1,18 @@
+defmodule Lucidex do
+  @moduledoc """
+  Documentation for `Lucidex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lucidex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lucidex_test.exs
@@ -0,0 +1,8 @@
+defmodule LucidexTest do
+  use ExUnit.Case
+  doctest Lucidex
+
+  test "greets the world" do
+    assert Lucidex.hello() == :world
+  end
+end
